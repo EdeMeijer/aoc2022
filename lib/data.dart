dd33d9c612fd09d630f9dbdb207b1f767758d364
Ede Meijer <ede@elastict.nl>
2022-12-08 11:38:21 +0100

    Day 5 part 1

--- lib/data.dart
+++ lib/data.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 Future<String> loadDataString(int day) async {
   final current = Directory.current;
   final dataFile = File('${current.path}/data/day$day');
-  return (await dataFile.readAsString()).trim();
+  return (await dataFile.readAsString()).trimRight();
 }
 
 Future<List<String>> loadDataLines(int day) async {
