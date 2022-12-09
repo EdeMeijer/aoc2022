93dc1a06611b83b22e5b32a29f1b3e4efb92997c
Ede Meijer <ede@elastict.nl>
2022-12-09 17:50:46 +0100

    Day 9 part 2

---
+++ bin/day9b.dart
@@ -0,0 +1,61 @@
+import 'package:aoc2022/data.dart';
+import 'package:aoc2022/utils.dart';
+
+Future<void> main() async {
+  var input = await loadDataLines(9);
+
+  var rope = [for (var _ in 0.until(10)) Coord(0, 0)];
+
+  var visited = {rope[9]};
+
+  for (final cmd in input.expand(parseCommand)) {
+    rope[0] += cmd;
+    for (var i = 1; i < 10; i ++) {
+      rope[i] = moveKnot(rope[i - 1], rope[i]);
+    }
+    visited.add(rope[9]);
+  }
+
+  print(visited.length);
+}
+
+const commands = {
+  'R': Coord(1, 0),
+  'U': Coord(0, -1),
+  'L': Coord(-1, 0),
+  'D': Coord(0, 1)
+};
+
+class Coord {
+  final int x, y;
+
+  const Coord(this.x, this.y);
+
+  Coord operator +(Coord other) => Coord(x + other.x, y + other.y);
+
+  Coord operator -(Coord other) => Coord(x - other.x, y - other.y);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Coord && x == other.x && y == other.y;
+
+  @override
+  int get hashCode => Object.hash(x, y);
+}
+
+Iterable<Coord> parseCommand(String line) sync* {
+  final parts = line.split(' ');
+  for (var i = 0; i < int.parse(parts[1]); i++) {
+    yield commands[parts[0]]!;
+  }
+}
+
+Coord moveKnot(Coord parent, Coord knot) {
+  var delta = parent - knot;
+
+  if (delta.x.abs() == 2 || delta.y.abs() == 2) {
+    return Coord(knot.x + delta.x.sign, knot.y + delta.y.sign);
+  } else {
+    return knot;
+  }
+}
